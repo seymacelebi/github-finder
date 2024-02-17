fb6d62f18f5ff2054ad1bf535be639f1eea557ca
Seyma CELEBI <celebiseyma98@gmail.com>
2024-02-17 23:28:03 +0300

    firt commit

---
+++ src/components/Search.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+export class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: "",
+    };
+  }
+  onChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.keyword === "") {
+      this.props.displayAlert("Anahtar kelime giriniz", "danger");
+    } else {
+      this.props.searchUsers(this.state.keyword);
+      this.setState({ keyword: "" });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <form onSubmit={this.onSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              value={this.state.keyword}
+              onChange={this.onChange}
+              className="form-control"
+              placeholder="Anahtar Kelime"
+            />
+            <button className="btn btn-primary" type="submit">
+              ARA
+            </button>
+          </div>
+        </form>
+
+        {this.props.showClearButton && (
+          <button
+            onClick={this.props.clearResults}
+            className="btn btn-outline-danger mt-2 btn-block"
+          >
+            Temizle
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Search;
